dec9e9f9e37cef451d0acd7ac15412f13ab24b6d
nnvuf <nnvu.fit@hotmail.com>
2017-10-04 13:31:57 +0700

    Add Check Spelling

---
+++ VietnameseSpelling/VietnameseSpelling/ClientApp/components/CheckSpelling.tsx
@@ -0,0 +1,66 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface iCheckSpelling {
+    words: WordPhrase;
+    loading: boolean;
+}
+
+export class CheckSpelling extends React.Component<RouteComponentProps<{}>, iCheckSpelling> {
+    constructor() {
+        super();
+        this.state = { words: { word: "Done...", isWord: true}, loading: true };
+
+        fetch('api/CheckSpelling/WordPhrases')
+            .then(response => response.json() as Promise<WordPhrase>)
+            .then(data => {
+                this.setState({ words: data, loading: false });
+            });
+    }
+
+    public SendWord() {
+        fetch('api/CheckSpelling/GetWord', {
+            method: "POST",
+            body: JSON.stringify({
+                Word: this.state.words.word
+            })
+        }).then(response => response.json() as Promise<WordPhrase>)
+            .then(data => {
+                this.setState({ words: data, loading: false });
+            });
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? "Loading..."
+            : this.state.words.word;
+        let isWord = this.state.loading
+            ? ""
+            : this.state.words.isWord
+            ? "Correct"
+            : "Error"
+        return <div>
+            <h1>Check Spelling</h1>
+
+            <p>Give me a Word:
+                <input type="text"
+                    value={this.state.words.word}
+                    onChange={(event) => this.updateState(event)} />
+                {isWord}
+            </p>
+
+            <button onClick={() => { this.SendWord() }}>Check</button>
+        </div>
+    }
+
+    public updateState(event: any) {
+        this.setState({ words: { word: event.target.value, isWord: this.state.words.isWord } });
+    }
+
+}
+
+interface WordPhrase {
+    word: string;
+    isWord: boolean;
+}
---
+++ VietnameseSpelling/VietnameseSpelling/Controllers/CheckSpellingController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace VietnameseSpelling.Controllers
+{
+    [Route("api/[controller]")]
+    public class CheckSpellingController : Controller
+    {
+        private static string[] sRandom = new[]
+        {
+            "Quyên"
+        };
+
+        private static CheckPhrase Check = new CheckPhrase();
+
+        [HttpGet("[action]")]
+        public WordPhrase WordPhrases()
+        {
+            var rng = new Random();
+            return new WordPhrase
+            {
+                Word = sRandom[rng.Next(sRandom.Length)],
+                IsWord = true
+            };
+        }
+
+        [HttpPost("[action]")]
+        public WordPhrase GetWord()
+        {
+            WordPhrase ret = new WordPhrase();
+            using (var ios = new StreamReader(Request.Body))
+            {
+                ret.Word = (new WordJson(ios.ReadToEnd())).Word;
+                ret.IsWord = Check.IsCorrect(ret.Word);
+            };
+            return ret;
+        }
+
+        public class WordPhrase
+        {
+            public string Word { get; set; }
+            public bool IsWord { get; set; }
+        }
+
+        public class WordJson
+        {
+            public string Word { get; set; }
+
+            public WordJson(string json)
+            {
+                JObject jObj = JObject.Parse(json);
+                JToken jToken = jObj;
+                Word = (string)jToken["Word"];
+            }
+        }
+    }
+
+    public class Word
+    {
+        public string Rhyme { get; set; }
+        public string Consonant { get; set; }
+    }
+
+    public class PatternWord
+    {
+        public string Rhyme { get; set; }
+        public string[] Consonant { get; set; }
+    }
+
+    public class CheckPhrase
+    {
+        private PatternWord[] cRhyme;
+        private string[] Consonant;
+        public CheckPhrase()
+        {
+            using (var isRhyme = new StreamReader("./Data/Rhyme.csv"))
+            {
+                List<PatternWord> tcRhyme = new List<PatternWord>();
+                while (!isRhyme.EndOfStream)
+                {
+                    var line = isRhyme.ReadLine();
+                    string[] value = line.Split(",");
+                    PatternWord temp = new PatternWord
+                    {
+                        Rhyme = value[0],
+                        Consonant = new string[value.Length - 1]
+                    };
+                    for (int ii = 1; ii < value.Length; ii++)
+                    {
+                        temp.Consonant[ii - 1] = value[ii];
+                    }
+                    tcRhyme.Add(temp);
+                }
+                cRhyme = tcRhyme.ToArray();
+            }
+            using (var isConsonant = new StreamReader("./Data/Consonant.csv"))
+            {
+                var line = isConsonant.ReadLine();
+                Consonant = line.Split(",");
+            }
+        }
+
+        public bool IsCorrect(string word)
+        {
+            string cWord = word.ToUpper();
+            Word checkRhyme = new Word()
+            {
+                Consonant = "",
+                Rhyme = ""
+            };
+            foreach (var Cons in Consonant)
+            {
+                if ((cWord.IndexOf(Cons) == 0) &&
+                    (checkRhyme.Consonant.Length < Cons.Length))
+                {
+                    checkRhyme.Consonant = Cons;
+                }
+            }
+            checkRhyme.Rhyme = cWord.Substring(checkRhyme.Consonant.Length);
+
+            PatternWord patternWord = new PatternWord();
+            foreach (var rRhyme in cRhyme)
+            {
+                if (checkRhyme.Rhyme.Equals(rRhyme.Rhyme))
+                {
+                    patternWord = rRhyme;
+                    break;
+                }
+            }
+            if ((new List<string>(patternWord.Consonant)).IndexOf(checkRhyme.Consonant) != -1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
